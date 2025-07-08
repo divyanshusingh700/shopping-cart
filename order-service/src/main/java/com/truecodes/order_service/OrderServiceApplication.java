eb608e5fed47b195df2cc91a90c9f7c28603c016
divyanshu2400 <divyanshu.1822en1171@kiet.edu>
2025-07-08 23:48:06 +0530

    Added discovery server and config for netflix eureka

--- order-service/src/main/java/com/truecodes/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/truecodes/order_service/OrderServiceApplication.java
@@ -2,12 +2,10 @@ package com.truecodes.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.truecodes.order_service.repository")
-@EntityScan(basePackages = "com.truecodes.order_service.model")
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
